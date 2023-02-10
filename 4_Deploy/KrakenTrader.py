cceaa595b6213d5c00822fcbbd54226c53cd2993
Illidan7 <shyamprabhakar92@gmail.com>
2023-02-10 12:29:58 -0600

    Deployment kickoff

---
+++ 4_Deploy/KrakenTrader.py
@@ -0,0 +1,133 @@
+import time
+import os
+import requests
+
+import urllib.parse
+import hashlib
+import hmac
+import base64
+import sys
+
+sys.path.append("/root/data/Config/")
+from config import *
+
+import pandas as pd
+
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+class KrakenTrader:
+    
+    def __init__(self):
+        self.api_url = "https://api.kraken.com"
+        self.fee_factor = 1 - FEES
+        self.offset = 100
+    
+    
+    def get_kraken_signature(self, urlpath, data, secret):
+
+        postdata = urllib.parse.urlencode(data)
+        encoded = (str(data['nonce']) + postdata).encode()
+        message = urlpath.encode() + hashlib.sha256(encoded).digest()
+
+        mac = hmac.new(base64.b64decode(secret), message, hashlib.sha512)
+        sigdigest = base64.b64encode(mac.digest())
+        return sigdigest.decode()
+    
+    
+    def kraken_request(self, uri_path, data):
+        headers = {}
+        headers['API-Key'] = API_KEY
+        # get_kraken_signature()
+        headers['API-Sign'] = self.get_kraken_signature(uri_path, data, SECRET_KEY)
+        req = requests.post((self.api_url + uri_path), headers=headers, data=data)
+        return req
+
+
+    def BUY(self, qty, price, symbol="XBTUSD"):
+        
+        resp = self.kraken_request('/0/private/AddOrder', {
+                                                        "nonce": str(int(1000*time.time())),
+                                                        "ordertype": "limit",
+                                                        "type": "buy",
+                                                        "volume": qty,
+                                                        "pair": symbol,
+                                                        "price": price
+                                                    })
+        
+        # return resp.json()['result']['ordernum']
+        return
+    
+    
+    def SELL(self, qty, price, symbol="XBTUSD"):
+        
+        resp = self.kraken_request('/0/private/AddOrder', {
+                                                        "nonce": str(int(1000*time.time())),
+                                                        "ordertype": "limit",
+                                                        "type": "sell",
+                                                        "volume": qty,
+                                                        "pair": symbol,
+                                                        "price": price
+                                                    })
+        
+        # return resp.json()['result']['ordernum']
+        return
+    
+    
+    def CHK_ORDER(self, ordernum):
+        pass
+    
+    
+    def GET_PRICE(self, symbol="XXBTZUSD"):
+
+        url = "https://api.kraken.com/0/public/Ticker?pair=XBTUSD"
+        response = requests.get(url)
+        data = response.json()
+        price = data['result'][symbol]['c'][0]
+        
+        return round(float(price),2)
+    
+    
+    def CHK_BTC(self):
+        resp = self.kraken_request('/0/private/Balance', {"nonce": str(int(1000*time.time()))})
+        return round(float(resp.json()['result']['XXBT']),8)
+    
+    
+    def CHK_CASH(self):
+        resp = self.kraken_request('/0/private/Balance', {"nonce": str(int(1000*time.time()))})
+        return round(float(resp.json()['result']['ZUSD']),2)
+    
+    
+    def CHK_BAL(self):
+        return self.CHK_BTC(), self.CHK_CASH()
+    
+    
+    def HIST_PRICE(self, symbol='XBTUSD', timeframe=60):
+        
+        resp = requests.get(f'https://api.kraken.com/0/public/OHLC?pair={symbol}&interval={timeframe}')
+
+        df = pd.DataFrame(resp.json()['result']['XXBTZUSD'], columns=['time', 'open', 'high', 'low', 'close', 'vwap', 'volume', 'count'])
+        df['time'] = pd.to_datetime(df['time'], unit='s')
+
+        return df
+    
+    
+    def CALC_MA(self, df, timeperiod):
+        df[f'MA{timeperiod}'] = df['close'].rolling(window=timeperiod).mean()
+        return df
+    
+    
+    def GET_MA(self, MA, symbol="XBTUSD", timeframe=60):
+        hist_df = self.HIST_PRICE(symbol=symbol, timeframe=timeframe)
+        MA1 = self.CALC_MA(hist_df, timeperiod=MA)
+        maxtime = MA1['time'].max()        
+        return MA1[MA1['time']==maxtime][f'MA{MA}'].values[0]
+    
+    
+    def TG_ALERT(self, alert):
+        requests.get(tg_baseurl+f'{alert}')
+
+        
+
+        
+
---
+++ 4_Deploy/MAEVE.py
@@ -0,0 +1,287 @@
+import KrakenTrader as krkn
+
+import pandas as pd
+
+import os
+import time
+import logging
+from datetime import datetime
+
+trader = krkn.KrakenTrader()
+
+save_loc = "/root/data/logs/"
+
+
+##################
+# Logging
+##################
+
+# Process logging
+log_loc = "/root/data/logs/MAEVE.log"
+logging.basicConfig(filename=log_loc, level=logging.INFO)
+
+
+####################
+# Custom functions
+####################
+
+# Event logging
+# datetime, price, usd, sats, MA12, MA20, cur_cash, profit/loss, cooldown
+def event_log(event):
+    event_df = pd.DataFrame(event)
+    path = save_loc + f"event_log.csv"
+    event_df.to_csv(path, mode='a', header=not os.path.exists(path), index=False)
+    return
+
+# Trade logging
+# datetime, trigger, tradetype, price, usd, sats, init_cash, cur_cash, profit/loss, streak, return
+def trade_log(trade):
+    trade_df = pd.DataFrame(trade)
+    path = save_loc + f"trade_log.csv"
+    trade_df.to_csv(path, mode='a', header=not os.path.exists(path), index=False)
+    return
+
+# Text logging
+def text_log(text, tg=True, padding=False):
+
+    if padding:
+        
+        logging.info("############################################")
+        logging.info(text)
+        logging.info("############################################")
+        
+        if tg:
+            trader.TG_ALERT("############################################")
+            trader.TG_ALERT(text)
+            trader.TG_ALERT("############################################")
+    else:
+        logging.info(text)
+        if tg:
+            trader.TG_ALERT(text)
+
+
+###########################
+# Strategy Implementation
+###########################
+
+logging.info("############################")
+logging.info(datetime.today())
+logging.info("MAEVE Starting")
+logging.info("############################")
+
+trader.TG_ALERT("############################################")
+trader.TG_ALERT("MAEVE Starting")
+trader.TG_ALERT("############################################")
+
+
+# Strategy parameters
+MAEVE = {
+            'MA1': 12,
+            'MA2': 20,
+            'stoploss': 0.01,
+            'streaklim': 2,
+            'cooldown': 48,
+            'trailing': True,
+            'sleepMin': 30,
+            'symbol': "BTC/USD"
+        }
+
+
+# Position management
+stop_price = 0
+orig_stop_price = 0
+streak = 0
+idle = 0
+stopped = 0
+
+
+
+while True:
+    
+    text = "Strategy Run @" + str(datetime.now())
+    text_log(text=text, tg=True, padding=True)
+    
+    ################
+    # Market pulse
+    ################
+     
+    text_log("Market pulse", tg=True, padding=False)
+    
+    # Get current price
+    price = trader.GET_PRICE()
+    # Get current balances
+    sats, usd = trader.CHK_BAL()
+    # Buy side
+    qty_buy = round(round(usd/(price+100), 8) * trader.fee_factor, 8)
+    qty_sell = round(sats * trader.fee_factor, 8)
+    
+    text = f"BTC price: {price}, USD: {usd}, BTC: {sats}, Total (USD): {round(usd + (sats*price), 2)}"
+    text_log(text=text, tg=True, padding=False)
+    
+    # Get MAs
+    MA1 = trader.GET_MA(MAEVE['MA1'])
+    MA2 = trader.GET_MA(MAEVE['MA2'])
+    
+    logging.info("Event Log")
+    # Event log
+    # datetime, price, usd, sats, MA12, MA20, cur_cash, profit/loss, cooldown
+    event = {
+                'datetime': [str(datetime.now())],
+                'price': [price],
+                'usd': [usd],
+                'sats': [sats],
+                'MA12': [MA1],
+                'MA20': [MA2],
+                'bullish': [MA1>MA2],
+                'stopped': [stopped],
+                'cur_cash': [round(usd + (sats*price), 2)],
+                'cooldown': [streak >= MAEVE['streaklim']] 
+            }
+    event_log(event)
+    
+    ############
+    # Cooldown
+    ############
+            
+    if streak >= MAEVE['streaklim']:
+        
+        text = f"Cooldown: {idle}"
+        text_log(text=text, tg=True, padding=True)
+        
+        idle += 1
+        if idle >= MAEVE['cooldown']:
+            streak = 0
+            idle = 0
+    
+    
+    #######################
+    # Position management
+    #######################
+    
+    # Trailing stop loss
+    if MAEVE['trailing'] and usd < 5:
+        
+        new_stop = round((1-MAEVE['stoploss']) * price, 2)
+        if stop_price == orig_stop_price:
+            if new_stop > (stop_price * (1+MAEVE['stoploss'])):
+                
+                stop_price = new_stop
+                text = f"Trailing Stop price -> break even -> {stop_price}"
+                text_log(text=text, tg=True, padding=True)
+                
+        else:
+            if new_stop > (stop_price * (1+0.01)):
+                
+                stop_price = new_stop
+                text = f"Trailing Stop price -> +0.01 -> {stop_price}"
+                text_log(text=text, tg=True, padding=True)
+                
+    
+    # Check stop loss trigger
+    if price < stop_price and usd < 5:
+        
+        text = f"Stop loss triggered at ${stop_price}"
+        text_log(text=text, tg=True, padding=True)
+        
+        # Update position
+        trader.SELL(qty=qty_sell, price=price-100, symbol="XBTUSD")
+        
+        # Update streak
+        streak += 1
+        stopped = 1
+        
+        logging.info("Trade log")
+        # Log trade
+        # datetime, trigger, tradetype, price, usd, sats, MA12, MA20, cur_cash, streak
+        trade = {
+                    'datetime': [str(datetime.now())],
+                    'trigger': ['STOP'],
+                    'tradetype': ['SELL'],
+                    'price': [price],
+                    'stopprice': [stop_price],
+                    'usd': [usd],
+                    'sats': [sats],
+                    'MA12': [MA1],
+                    'MA20': [MA2],
+                    'cur_cash': [round(usd + (sats*price), 2)],
+                    'streak': [streak] 
+                }
+        trade_log(trade)
+        
+    ###############
+    # BUY signal
+    ###############
+
+    # Check if the MA1 is higher than the MA2
+    if MA1 > MA2:
+        
+        if usd > 5:
+            
+            text = f"BUY signal triggered at ${price}"
+            text_log(text=text, tg=True, padding=True)
+            
+            # Update position
+            trader.BUY(qty=qty_buy, price=price+100, symbol="XBTUSD")
+        
+            # Position management
+            orig_stop_price = round((1-MAEVE['stoploss']) * price, 2)
+            stop_price = round((1-MAEVE['stoploss']) * price, 2)
+            stopped = 0
+
+            logging.info("Trade log")
+            # Log trade
+            # datetime, trigger, tradetype, price, usd, sats, MA12, MA20, cur_cash, streak
+            trade = {
+                        'datetime': [str(datetime.now())],
+                        'trigger': ['BUY'],
+                        'tradetype': ['BUY'],
+                        'price': [price],
+                        'stopprice': [stop_price],
+                        'usd': [usd],
+                        'sats': [sats],
+                        'MA12': [MA1],
+                        'MA20': [MA2],
+                        'cur_cash': [round(usd + (sats*price), 2)],
+                        'streak': [streak]
+                    }
+            trade_log(trade)
+
+    ###############
+    # SELL signal
+    ###############
+
+    # Check if the MA1 is lower than the MA2
+    elif MA1 < MA2:
+        # If we're currently holding BTC, sell
+        if usd < 5:
+            
+            text = f"SELL signal triggered at ${price}"
+            text_log(text=text, tg=True, padding=True)
+            
+            # Update position
+            trader.SELL(qty=qty_sell, price=price-100, symbol="XBTUSD")
+            
+            stopped = 0
+            
+            logging.info("Trade log")
+            # Log trade
+            # datetime, trigger, tradetype, price, usd, sats, MA12, MA20, cur_cash, streak
+            trade = {
+                        'datetime': [str(datetime.now())],
+                        'trigger': ['SELL'],
+                        'tradetype': ['SELL'],
+                        'price': [price],
+                        'stopprice': [stop_price],
+                        'usd': [usd],
+                        'sats': [sats],
+                        'MA12': [MA1],
+                        'MA20': [MA2],
+                        'cur_cash': [round(usd + (sats*price), 2)],
+                        'streak': [streak] 
+                    }
+            trade_log(trade)
+
+    text = f"Sleeping for {MAEVE['sleepMin']} min"
+    text_log(text=text, tg=True, padding=False)
+    
+    time.sleep(MAEVE['sleepMin']*60)
